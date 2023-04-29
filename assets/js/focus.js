b1e235d95193f18eb776e5054e37dd6ce229da26
erick.jung <erick.jung@farfetch.com>
2023-04-29 22:27:29 +0100

    updating main page

--- assets/js/focus.js
+++ assets/js/focus.js
@@ -266,12 +266,12 @@ $(function() {
 function buzzer() {
     // _gaq.push(['_trackEvent', 'buzzcheck', document.getElementById("check_audio").checked]);
     
-    $.cookie('audioCheck', document.getElementById("check_audio").checked);
+    // $.cookie('audioCheck', document.getElementById("check_audio").checked);
 
-    if (document.getElementById("check_audio").checked) {
+    // if (document.getElementById("check_audio").checked) {
         var b = new Audio("assets/audio/alarm.wav");
         b.load();
         b.play();
-    }
+    // }
 };
 
